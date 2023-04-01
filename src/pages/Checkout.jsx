5fafd5e7b908c9e0f4876e2ee1a507d744ca156a
ElamanZ <z.elaman699@gmail.com>
2023-04-01 19:05:20 +0600

    tenth day

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,11 +1,65 @@
-
-
-import React from 'react'
-
+import React from "react";
+import { useSelector } from "react-redux";
+import { Container, Row, Col } from "reactstrap";
+import CommonSection from "../components/UI/common-section/CommonSection";
+import Helmet from "../components/Helmet/Helmet";
+import "../styles/checkout.css"
 const Checkout = () => {
-  return (
-    <div>Checkout</div>
-  )
-}
+  const cartTotalAmount = useSelector((state) => state.cart.totalAmount);
+  const shippingCost = 30;
 
-export default Checkout
+  const totalAmount = cartTotalAmount + Number(shippingCost);
+  return (
+    <Helmet title="Checkout">
+      <CommonSection title="Checkout" />
+      <section>
+        <Container>
+          <Row>
+            <Col lg="8" md="6">
+              <h6 className="mb-4">Shipping Address</h6>
+              <form className="checkout__form">
+                <div className="form__group">
+                  <input type="text" placeholder="Enter your name" />
+                </div>
+                <div className="form__group">
+                  <input type="email" placeholder="Enter your email" />
+                </div>
+                <div className="form__group">
+                  <input type="number" placeholder="Phone number" />
+                </div>
+                <div className="form__group">
+                  <input type="text" placeholder="Country" />
+                </div>
+                <div className="form__group">
+                  <input type="text" placeholder="City" />
+                </div>
+                <div className="form__group">
+                  <input type="number" placeholder="Postal code" />
+                </div>
+              </form>
+              <button className="addTOcart__btn">Payment</button>
+            </Col>
+
+            <Col lg="4" md="6">
+              <div className="checkout__bill">
+                <h6>
+                  Subtotal: <span>${cartTotalAmount}</span>
+                </h6>
+                <h6>
+                  Subtotal: <span>${shippingCost}</span>
+                </h6>
+                <div>
+                  <h5>
+                    Total: <span>${totalAmount}</span>
+                  </h5>
+                </div>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </Helmet>
+  );
+};
+
+export default Checkout;
